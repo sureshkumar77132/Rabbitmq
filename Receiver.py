c1170de9dfc0f6111d7015abed8e1dd437b06db0
SureshkumarAnbazhagan <46334330+sureshkumar77132@users.noreply.github.com>
2020-06-04 11:25:51 +0530

    Update Receiver.py

--- Receiver.py
+++ Receiver.py
@@ -1,19 +1,19 @@
 try:
-    import pika
+   import pika
 except Exception as e:
-    print("some modules missing {}".format_map(e))
+   print("some modules missing {}".format_map(e))
 class MetaClass(type):
-    _instance={}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instance:
-            cls._instance[cls]=super(MetaClass, cls).__call__(*args,**kwargs)
-            return cls._instance[cls]
+   _instance={}
+   def __call__(cls, *args, **kwargs):
+         if cls not in cls._instance:
+             cls._instance[cls]=super(MetaClass, cls).__call__(*args,**kwargs)
+             return cls._instance[cls]
 class RabbitMqServerConfigure():
     def __init__(self,host='localhost',queue='hello'):
-        self.host=host
-        self.queue=queue
+         self.host=host
+         self.queue=queue
 class rabbitmqServer():
-    def __init__(self, server):
+   def __init__(self, server):
         self.server=server
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel=connection.channel()
@@ -21,7 +21,7 @@ result=channel.queue_declare(queue='hello')
 queue_name = result.method.queue
 channel.queue_bind(exchange='logss', queue=queue_name)
 def callback(ch,method,properties,body):
-    print(" [x] received %r" %body)
+     print(" [x] received %r" %body)
 channel.basic_consume(queue='hello',on_message_callback=callback,auto_ack=True)
 print('[*] waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
